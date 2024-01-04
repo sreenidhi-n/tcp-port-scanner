962818505a20039c6eac0a4208ee2557683e2dd8
sreenidhi nallajerla <95966587+sreenidhi-n@users.noreply.github.com>
2024-01-04 20:13:04 +0530

    Add files via upload

---
+++ scanner.py
@@ -0,0 +1,52 @@
+import socket
+from termcolor import colored
+
+COMMON_PORTS = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    135: "RPC",
+    139: "NetBIOS",
+    143: "IMAP",
+    443: "HTTPS",
+    445: "SMB",
+    1433: "MSSQL",
+    3306: "MySQL",
+    3389: "RDP",
+    5900: "VNC"
+}
+
+def check_port(host, port):
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(1) # timeout to prevent the connection from hanging indefinitely
+        result = client.connect_ex((host, port))
+        if result == 0: # if connection is successful
+            if port in COMMON_PORTS: # check if the port corresponds to a common service
+                service = COMMON_PORTS[port]
+                print(colored(f"[+] Port {port} ({service}): Open", "green"))
+            else:
+                print(colored(f"[+] Port {port}: Open", "green"))
+        client.close()
+
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit()
+
+    except socket.gaierror:
+        print("Host name could not be resolved.")
+        exit()
+
+    except socket.error:
+        pass
+
+target_host = input("Enter host IP address: ")
+min_port = int(input("Enter the minimum port number: "))
+max_port = int(input("Enter the maximum port number: "))
+
+for port in range(min_port, max_port+1):
+    check_port(target_host, port)
